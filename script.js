e54d4c156b4b782ce7cf03bd2f36dec27d325a3c
luqmankh56 <luqman.kh5658@gmail.com>
2024-07-26 01:45:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLeads =[]
+const inputEl =document.getElementById("input-el")
+const inputBtn=document.getElementById("input-btn")
+const ulEl =document.getElementById("ul-el")
+
+
+inputBtn.addEventListener("click",function() {
+    myLeads.push (inputEl.value)
+    inputEl.value= ""
+    console.log (myLeads)
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems =""
+    for(let i=0; i < myLeads.length;i++) {
+    console.log(myLeads[i])}
+    }
